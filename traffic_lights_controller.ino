8c591d475d59ddc44fdd6fbfbccc9b559368fa17
xram255 <amalgmg25@gmail.com>
2020-07-11 10:28:45 +0530

    Before removing libraries

--- traffic_lights_controller.ino
+++ traffic_lights_controller.ino
@@ -3,11 +3,13 @@
 
 #define CYCLE_TIME 30
 #define SER_RD_TME 20
-#define MUX_DELAY 3000
 
-int t1 = 10, t2 = 0, t3 = 0, t4, t5, t6;
-int8_t inc = 0;
-uint32_t cycle_timer = 0, ser_rd_tmr = 0, mux_tmr = 0;
+uint16_t cd_delay = 1000;
+int t1 = 8, t2 = 8, t3 = 8, t4 = 8, t5 = 8, t6 = 8;
+int8_t inc = 0, cd_state = 1;
+uint32_t cycle_timer = 0, ser_rd_tmr = 0, mux_tmr = 0, cd_tmr;
+
+int cd_var = t1;
 
 LightsClass lights(0);
 SegmentsClass seg;
@@ -16,44 +18,38 @@ boolean ser_avl = 0;
 void setup()
 {
 	Serial.begin(9600);
+
 	cycle_timer = millis();
 	ser_rd_tmr = millis();
 	mux_tmr = micros();
+	
 }
 
 void loop()
 {
+	
 	inReader();
 	cycle_update(CYCLE_TIME);
-	//seg.display_on(t1, t2, t3);
-	//lights.state_config(4, 6, 10);
+	//seg.display_on(cd_var, cd_var, cd_var);
+	lights.state_config(t1, t2, t3);
+	if (cd_state == 1)
+	{
+		seg.display_on(cd_var, cd_var, cd_var + t2);
+	}
+	else if (cd_state == 2)
+	{
+		seg.display_on(cd_var, cd_var, cd_var);
+	}
+	else if (cd_state == 3)
+	{
+		seg.display_on(cd_var + t1, cd_var + t1, cd_var);
+	}
+
+	count_down_tmr();
 }
 
 void inReader()
 {
-	/*
-	if (Serial.available())
-	{
-		delay(20);
-		if (inc == 1)
-		{
-			t1 = inStringReader().toInt();
-			
-			//inc++;
-		}
-		else if (inc == 2)
-		{
-			t2 = inStringReader().toInt();
-			//inc++;
-		}
-		else if (inc == 3)
-		{
-			t3 = inStringReader().toInt();
-			//inc = 1;
-		}
-	}
-	*/
-	
 	if (inc == 1)
 	{
 		t4 = inStringReader().toInt();
@@ -109,19 +105,44 @@ void cycle_update(uint16_t upd_time) // update t1, t2, t3 for every entered valu
 		t1 = t4;
 		t2 = t5;
 		t3 = t6;
+
 		//Serial.println("cyc tme");
 	}
 }
 
-boolean mux_config()
+uint16_t count_down_tmr()
 {
-	if (micros() - mux_tmr > MUX_DELAY)
+	if (millis() - cd_tmr > cd_delay)
 	{
-		mux_tmr = micros();
-		return 1;
+		cd_tmr = millis();
+		cd_var--;
+		//Serial.println(cd_var);
+		//Serial.println(cd_tmr);
+		cd_delay = 1000;
 	}
-	else
+	if (cd_var < 1 )
 	{
-		return 0;
+		if (cd_state == 1)
+		{
+			cd_var = t2 ;
+			cd_state = 2;
+			cd_delay = 3000;
+			
+		}
+		else if (cd_state == 2)
+		{
+			cd_var = t3 ;
+			cd_state = 2;
+			cd_delay = 3000;
+			
+		}
+		else if (cd_state == 3)
+		{
+			cd_var = t1 ;
+			cd_state = 1;
+			cd_delay = 3000;
+			
+		}	
 	}
 }
+
